2f0e6eee89b70e80d26f9125c6dd4cae764de0f0
marcels123 <2124642@brunel.ac.uk>
2025-02-18 03:52:35 +0000

    Added new dependencies + changed css in navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,20 +1,21 @@
-// Navigation bar
+// // Navigation bar
 
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 p-4 text-white flex justify-between items-center">
+    <nav className="bg-blue-600 p-4 text-white flex justify-between items-center w-screen">
       <Link href="/" className="text-xl font-bold">
         AI & Digital Media
       </Link>
-      <div className="space-x-4">
-      <Link href="/">Home</Link>
-      <Link href="/site/media/applications">Applications</Link>
-      <Link href="/media/history">History</Link>
-      <Link href="/account/profile">My Account</Link>
-      <Link href="/authentication/signin">Sign In</Link>
+      <div className="flex flex-grow justify-evenly">
+        <Link href="/" className="px-4">Home</Link>
+        <Link href="/site/media/applications" className="px-4">Applications</Link>
+        <Link href="/media/history" className="px-4">History</Link>
+        <Link href="/account/profile" className="px-4">My Account</Link>
+        <Link href="/authentication/signin" className="px-4">Sign In</Link>
       </div>
     </nav>
   );
 }
+
